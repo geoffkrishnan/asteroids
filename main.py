404daf93ee103500c0de5bddc4128f6683d445f4
Geoff Krishnan <geoffrykrishnan@gmail.com>
2025-10-02 14:20:34 -0500

    ability to render player(ship) obj to screen

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    # Screen
     while True:
+        #  so the game quits properly when you click close
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        dt_ms = clock.tick(60)
-        dt = dt_ms / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
