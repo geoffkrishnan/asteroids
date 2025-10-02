44f5d1f1c7c194db0ce58eb88655eb859bf6a901
Geoff Krishnan <geoffrykrishnan@gmail.com>
2025-10-02 09:08:36 -0500

    limited framerate to max 60 FPS

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt_ms = clock.tick(60)
+        dt = dt_ms / 1000
 
 
 if __name__ == "__main__":
